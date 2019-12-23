c47b01fcb7d20ff0396bab4c77b0e36b94924677
kowalix007 <kowalix007@gmail.com>
2019-12-23 09:07:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,17 @@
 class Dog 
   def name= (dogs_name) 
-    this_dog_name = dog_name 
+    @this_dogs_name = dogs_name 
   end
   
   def name 
-    this_dog_name
+    @this_dog_name
   
   end 
 end 
+
+fido = Dog.new 
+fido.name = "Fido" 
+
+fido.name 
+fido.bark = "Woof" 
+fido.bark
